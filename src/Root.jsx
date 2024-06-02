bde20b8f57e9ffa45b4b5e5d909c3248e3e1b2f2
robiul99siyam <siyamrobiulislam@gmail.com>
2024-06-02 19:11:29 +0600

    added footer

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from './Pages/Footer/Footer';
 
 const Root = (props) => {
     return (
         <div>
             navbar
             <Outlet/>
-            footer
+            <Footer/>
         </div>
     );
 };
